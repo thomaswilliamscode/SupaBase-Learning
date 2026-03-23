1bef77b809c56a4e5ba4419890b16b3f95dbef5c
Thomas Williams <thomas.williams.code@gmail.com>
2026-03-23 15:32:09 -0400

    remove ts

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
