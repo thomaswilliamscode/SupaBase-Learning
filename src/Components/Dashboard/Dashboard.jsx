2f287dd2a1d67dde64a9ec533875665f889cdfd9
Thomas Williams <thomas.williams.code@gmail.com>
2026-03-24 10:57:34 -0400

    test

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -8,19 +8,7 @@ function Dashboard () {
 
     useEffect( () => {
 
-        async function fetchMetrics () {
-        const { data, error } = await supabase
-            .from('sales_deals') // defaults to public
-            .select('*');        // fetch all columns
-            const newData = { data, error}
-            return newData
-    }
-    
-        const load = async () => {
-            const data = await fetchMetrics();
-            console.log(data)
-        }
-        load()
+        
         console.log(load())
         console.log(import.meta.env.VITE_SUPABASE_URL);
         console.log(import.meta.env.VITE_SUPABASE_KEY);
